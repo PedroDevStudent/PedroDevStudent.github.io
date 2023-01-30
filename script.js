805e01edbe3d2c4f7947ab8e377cfd0f4039ec14
Pedro Furigo <pedrofurigo.dev@hotmail.com>
2023-01-30 12:06:20 -0300

    Topnav Content

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Working?')
+function responsiveNavbar() {
+    var navBar = document.querySelector(".topnav");
+    console.log("Tá okay")
+    if (navBar.className === "topnav") {
+        navBar.className += " responsive";
+    } else {
+        navBar.className = "topnav";
+    }
+}
